f56530e2d2af98610646298771af1c1c11b38d55
mahan <mdpe84.ir@gmail.com>
2026-02-27 11:15:26 +0330

    Fix url laaunher for the web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1748751534" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "80001551" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
