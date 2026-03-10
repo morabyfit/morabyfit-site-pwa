302de1a794548b05bd6badb81ec857c9cb99260d
mahan <mdpe84.ir@gmail.com>
2026-03-10 16:38:38 +0330

    replaced font with svg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1375384854" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2625277476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
